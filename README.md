4cdbfa4041430c79bdf9ced509923b530e558bf9
Michael Alekseew <michael.alekseew@sovanta.com>
2022-04-26 13:38:40 +0200

    feat(transform): add palindrome function and helpers

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # algorithm-challenge
+
 An algorithm challenge that I try to solve / implement in around 2 hours.
--- src/Transform.class.ts
+++ src/Transform.class.ts
@@ -1,6 +1,37 @@
 export class Transform {
   public static palindrome(N: number): number {
-    return N;
+    if (Transform.isPalindrome(N)) {
+      return N;
+    } else {
+      const minInput = 1;
+      const maxInput = 10000;
+      if (N < minInput || N > maxInput) {
+        console.error(
+          `${N} is not in the allowed range of ${minInput} to ${maxInput}`
+        );
+        return -1;
+      } else {
+        return Transform.recursionPalindrome(N, N);
+      }
+    }
+  }
+
+  private static recursionPalindrome(
+    N: number,
+    initialN: number,
+    count = 1
+  ): number {
+    const maxValue = 1000000000;
+    const rev = Transform.reverseNumber(N);
+    const sum = N + rev;
+    if (sum > maxValue) {
+      console.error(
+        `The palindrome could not be found for ${initialN} (searched until ${maxValue} and had ${count} turns).`
+      );
+      return -1;
+    } else {
+      return Transform.recursionPalindrome(sum, initialN, count + 1);
+    }
   }
 
   public static isPalindrome(N: number): boolean {
@@ -11,5 +42,14 @@ export class Transform {
       }
     }
     return true;
+  }
+
+  public static reverseNumber(N: number): number {
+    const S = String(N);
+    let rev = '';
+    for (let i = S.length - 1; i >= 0; i--) {
+      rev += S[i];
+    }
+    return Number(rev);
   }
 }
