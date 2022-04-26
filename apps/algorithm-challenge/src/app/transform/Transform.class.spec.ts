a695817dbd198613d56aaa29fa123c9b9e4f99da
Michael Alekseew <michael.alekseew@sovanta.com>
2022-04-26 22:14:38 +0200

    test(transform): add test for transform isPalindrome

--- apps/algorithm-challenge/src/app/transform/Transform.class.spec.ts
+++ apps/algorithm-challenge/src/app/transform/Transform.class.spec.ts
@@ -11,4 +11,18 @@ describe('Transform Class', () => {
     const result3 = Transform.reverseNumber(1234);
     expect(result3).toBe(4321);
   });
+
+  it('should return true if given number is palindrome', () => {
+    const result = Transform.isPalindrome(123);
+    expect(result).toBe(false);
+
+    const result2 = Transform.isPalindrome(121);
+    expect(result2).toBe(true);
+
+    const result3 = Transform.isPalindrome(1234);
+    expect(result3).toBe(false);
+
+    const result4 = Transform.isPalindrome(1221);
+    expect(result4).toBe(true);
+  });
 });
