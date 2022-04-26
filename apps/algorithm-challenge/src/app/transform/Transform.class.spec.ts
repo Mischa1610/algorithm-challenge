1bdd5f3b5377a5a9c4348a00734692eb19b5cada
Michael Alekseew <michael.alekseew@sovanta.com>
2022-04-26 23:50:34 +0200

    test(transform): adapt test to new output format of palindrome function

--- apps/algorithm-challenge/src/app/transform/Transform.class.spec.ts
+++ apps/algorithm-challenge/src/app/transform/Transform.class.spec.ts
@@ -31,24 +31,31 @@ describe('Transform Class', () => {
 
   it('should return the palindrome of given number', () => {
     const result = Transform.palindrome(28);
-    expect(result).toBe(121);
+    expect(result.palindrome).toBe(121);
+    expect(result.recursionCount).toBe(2);
 
     const result2 = Transform.palindrome(34543);
-    expect(result2).toBe(34543);
+    expect(result2.palindrome).toBe(34543);
+    expect(result2.recursionCount).toBe(1);
 
     const result3 = Transform.palindrome(196);
-    expect(result3).toBe(-1);
+    expect(result3.palindrome).toBe(-1);
+    expect(result3.recursionCount).toBe(17);
 
     const result4 = Transform.palindrome(1221);
-    expect(result4).toBe(1221);
+    expect(result4.palindrome).toBe(1221);
+    expect(result4.recursionCount).toBe(1);
 
     const result5 = Transform.palindrome(51);
-    expect(result5).toBe(66);
+    expect(result5.palindrome).toBe(66);
+    expect(result5.recursionCount).toBe(1);
 
     const result6 = Transform.palindrome(11);
-    expect(result6).toBe(11);
+    expect(result6.palindrome).toBe(11);
+    expect(result6.recursionCount).toBe(1);
 
     const result7 = Transform.palindrome(607);
-    expect(result7).toBe(4444);
+    expect(result7.palindrome).toBe(4444);
+    expect(result7.recursionCount).toBe(2);
   });
 });
